ba8f34f69e20e4b3e27002ef2fc6ce7d7a4f94ce
Kevin Wong <kevinwong@pursuit.org>
2020-05-23 18:56:21 -0400

    fix bugs in queries and routes, test on postman

--- backend/queries/submemmitsQueries.js
+++ backend/queries/submemmitsQueries.js
@@ -1,70 +1,81 @@
 const db = require("../db/index");
 
-const getAllSubmemmits = (req, res, next) => {
+const getAllSubmemmits = async (req, res, next) => {
   try {
     res.status(200).json({
       status: "Success",
       message: "get ALL submemmits",
       body: {
-          submemmits: await db.any("SELECT * FROM submemmits")
+        submemmits: await db.any("SELECT * FROM submemmits"),
       },
     });
   } catch (error) {
     res.json({
-        error: error,
-    })
+      error: error,
+    });
   }
 };
 
-const getSingleSubmemmitById = (req, res, next) => {
-    try{
-        const {id} = req.params;
-        res.status(200).json({
-            status: "Success",
-            message: "got single submemmit by id: " + id,
-            body: {
-                submemmit: await db.one('SELECT * FROM submemmits WHERE id = $1', id)
-            }
-        })
-    } catch(error){
-        res.json({
-            error: error,
-        })
-    }
-}
-
-const insertSingleSubmemmit = (req, res, next) => {
-    try {
-        const {name, owner_id} = req.body;
-        res.status(200).json({
-          status: "Success",
-          message: "get ALL submemmits",
-          body: {
-              submemmits: await db.any('INSERT INTO submemmits (name, owner_id) VALUES ($1, $2) RETURNING *', [name, owner_id])
-          },
-        });
-      } catch (error) {
-        res.json({
-            error: error,
-        })
-      }
-}
-
-const deleteSingleSubmemmit = (req, res, next) => {
-    try {
-        const {id} = req.params;
-        res.status(200).json({
-            status: "Success",
-            message: "Deleted submemmit with id: " + id,
-            body: {
-                single_submemmit: await db.one('DELETE FROM submemmits WHERE id = $1 RETURNING *', id)
-            }
-        });
-    } catch (error) {
-        res.json({
-            error: error,
-        })
+const getSingleSubmemmitById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    res.status(200).json({
+      status: "Success",
+      message: "got single submemmit by id: " + id,
+      body: {
+        submemmit: await db.one("SELECT * FROM submemmits WHERE id = $1", id),
+      },
+    });
+  } catch (error) {
+    res.json({
+      error: error,
+    });
   }
-}
+};
 
-module.exports = { getAllSubmemmits, getSingleSubmemmitById, insertSingleSubmemmit, deleteSingleSubmemmit }
+const insertSingleSubmemmit = async (req, res, next) => {
+  try {
+    const { name, owner_id } = req.body;
+    res.status(200).json({
+      status: "Success",
+      message: "get ALL submemmits",
+      body: {
+        submemmits: await db.any(
+          "INSERT INTO submemmits (name, owner_id) VALUES ($1, $2) RETURNING *",
+          [name, owner_id]
+        ),
+      },
+    });
+  } catch (error) {
+    res.json({
+      error: error,
+    });
+  }
+};
+
+const deleteSingleSubmemmit = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    res.status(200).json({
+      status: "Success",
+      message: "Deleted submemmit with id: " + id,
+      body: {
+        single_submemmit: await db.one(
+          "DELETE FROM submemmits WHERE id = $1 RETURNING *",
+          id
+        ),
+      },
+    });
+  } catch (error) {
+    res.json({
+      error: error,
+    });
+  }
+};
+
+module.exports = {
+  getAllSubmemmits,
+  getSingleSubmemmitById,
+  insertSingleSubmemmit,
+  deleteSingleSubmemmit,
+};
